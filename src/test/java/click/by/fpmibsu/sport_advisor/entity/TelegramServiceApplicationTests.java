d74eb9c5f5b54bcbe01bd354d54364b122c12959
Kavazar <zaharkalosha@gmail.com>
2023-05-26 21:31:41 +0300

    Add tests template

---
+++ src/test/java/click/by/fpmibsu/sport_advisor/entity/TelegramServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package click.by.fpmibsu.sport_advisor.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramServiceApplicationTests {
+
+}

9da54fa11559b35a33c94cd93a3443a20fd88340
Kavazar <kalosha.zahar@student.ehu.lt>
2023-05-26 18:49:46 +0300

    Add application "main" class

---
+++ src/main/java/click/by/fpmibsu/sport_advisor/entity/AdviserApplication.java
@@ -0,0 +1,13 @@
+package click.by.fpmibsu.sport_advisor.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdviserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdviserApplication.class, args);
+    }
+
+}
